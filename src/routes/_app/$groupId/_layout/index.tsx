93b9a59d53a075caf7e07e0dfba0efa204233572
Billy Hawkes <billyhawkes02@gmail.com>
2024-07-05 10:37:00 -0400

    Fixed userlist reference

--- src/routes/_app/$groupId/_layout/index.tsx
+++ src/routes/_app/$groupId/_layout/index.tsx
@@ -1,4 +1,3 @@
-import { api } from "@/trpc/react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/$groupId/_layout/")({
@@ -6,7 +5,5 @@ export const Route = createFileRoute("/_app/$groupId/_layout/")({
 });
 
 function Index() {
-	const { data } = api.userList.useQuery();
-
 	return <div className="p-8"></div>;
 }
