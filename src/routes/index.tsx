58b58325e1bb2e241c91ff87eafdbe01d04b5d57
Billy Hawkes <billyhawkes02@gmail.com>
2024-07-18 13:55:51 -0400

    Added branding

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
+import { Users } from "lucide-react";
 
 export const Route = createFileRoute("/")({
 	component: Index,
@@ -9,7 +10,10 @@ function Index() {
 	return (
 		<div className="flex flex-1 justify-center items-center flex-col gap-8 p-4">
 			{/* Site description */}
-			<h1 className="text-center">All-In-One Group Organizer</h1>
+			<div className="bg-black p-4 rounded-xl">
+				<Users size={100} className="text-white" />
+			</div>
+			<h1 className="text-center">All-In-One Group Planner</h1>
 			<p className="text-center">
 				This app lets you chat, share photos, and plan events with anyone all in one place
 			</p>
